a841f5a6b18a0ddeffcce2e75c3a08a33574db10
ansari14 <faizanansari.1692@gmail.com>
2025-07-17 14:45:07 +0530

    Fix Angular standalone components and routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserList } from './components/user-list/user-list.component';
+import { UserForm } from './components/user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UserList },
+  { path: 'add-user', component: UserForm },
+  { path: 'edit-user/:id', component: UserForm }
+];
